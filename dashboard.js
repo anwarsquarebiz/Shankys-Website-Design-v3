7a76a28011010ac111112f4e0b948eb64842e1c9
Anwar Shaikh <anwar.squarebiz@gmail.com>
2024-01-23 00:32:38 +0530

    Coverting dashboard to mobile

--- dashboard.js
+++ dashboard.js
@@ -8,3 +8,11 @@ function togglepasswordchange() {
         passwordChangeOption.style.display = "none";
     }
 }
+
+// Toggle mobile menu
+function toggleHideOnMobile() {
+    console.log('Toggle Hide On Mobile');
+    const sideNavbar = document.getElementById('side-navbar');
+    sideNavbar.classList.toggle('hide-on-mobile');
+    console.log('Toggle Hide On Mobile');
+}
